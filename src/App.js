ea306362e6948b60316c45138a20da03ccfa4683
Sergii Tanchenko <serg.tanchenko@gmail.com>
2019-01-08 23:14:40 +0200

    add change name handler

--- src/App.js
+++ src/App.js
@@ -12,17 +12,39 @@ class App extends Component {
   }
 
   switchNameHandler = (newName) => {
-    // TODO: updated the state 
+    this.setState({
+      persons: [
+        { name: "Victor", age: "27", hobbies: ["Racing"] },
+        { name: newName, age: "18" },
+        { name: "Abcde", age: "12" }
+      ]
+    });
+  }
+
+  onChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Victor", age: "27", hobbies: ["Racing"] },
+        { name: event.target.value, age: "18" },
+        { name: "Abcde", age: "12" }
+      ]
+    });
   }
 
   render() {
     const persons = this.state.persons.map(
-      (person) => <Person name={person.name} age={person.age}>{person.hobbies}</Person>
+      (person) => <Person
+        name={person.name}
+        age={person.age}
+        changed={this.onChangeHandler}
+      >
+        {person.hobbies}
+      </Person >
     );
 
     return (
       <div className="App">
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, "Anonimous")}>Switch Name</button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    const { name, age, children } = props;
+    const { name, age, children, changed } = props;
     return (
         <div>
             <p>I'm {name} and I'm {age} years old</p>
             {children && <p>{children}</p>}
+            <input type="text" onChange={changed} value={name} />
         </div>
     );
 };
